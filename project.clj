cb44bc04cd0296fd42c5b569ff50e94f39503da9
Robert Marianski <rob@marianski.com>
2012-05-31 20:53:22 -0400

    add url to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject rmarianski/servlet-session-store "0.0.1"
   :description "Servlet based session store implementation"
+  :url "https://github.com/rmarianski/servlet-session-store"
   :dependencies [[ring/ring-core "1.1.0"]])
